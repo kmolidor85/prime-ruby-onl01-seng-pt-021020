4b8a11a5f7aa75a598425d766555be89aabf00cd
kyle molidor <k_molidor@yahoo.com>
2020-02-23 00:41:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(number)
   start = 2
   if number > 1
-    range = (start..number-1).to_a
+    range = (start..number-1).to
     range.none? do |num_to_test|
 
       number % num_to_test == 0
